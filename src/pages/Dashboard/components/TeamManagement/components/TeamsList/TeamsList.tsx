c2b11437fcd50e22386f9f1d1d64fea7d7a87596
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-07-27 22:38:41 -0400

    added delete confirm modal

--- src/pages/Dashboard/components/TeamManagement/components/TeamsList/TeamsList.tsx
+++ src/pages/Dashboard/components/TeamManagement/components/TeamsList/TeamsList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Card, Col, Stack, Row } from "react-bootstrap";
+import { Button, Card, Col, Modal, Row } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useGoldCupApi } from "../../../../../../hooks";
 
@@ -7,6 +7,7 @@ export const TeamsList = () => {
     const {getManagedTeams, createCookieObject, deleteTeam} = useGoldCupApi();
     const navigate = useNavigate()
     const [teams, setTeams] = useState<any[]>([]);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
 
     useEffect(() => {
         console.log('useEffect TeamsList');
@@ -36,22 +37,37 @@ export const TeamsList = () => {
             const row = teams.slice(i, i + rowSize)
             const rowItems = row.map((team, index) => {
             return (
+                <>
                     <Col key={index} md={4}>
                         <Card>
                             <Card.Body>
                                 <Card.Title>{team.name}</Card.Title>
                                 <Card.Text>Division: {team.division}</Card.Text>
-                                <Stack gap={3} direction='horizontal'>
-
-                                </Stack>
                             </Card.Body>
                             <Card.Footer>
                                 <Card.Link onClick={() => navigate(`/team/${team.id}`)}>View</Card.Link>
                                 <Card.Link onClick={() => navigate(`/team/${team.id}/edit`)}>Edit</Card.Link>
-                                <Card.Link className="link-danger" onClick={() => handleDelete(team.id)}>Delete</Card.Link>
+                                <Card.Link className="link-danger" onClick={() => setShowDeleteModal(true)}>Delete</Card.Link>
                             </Card.Footer>
                         </Card>
                     </Col>
+                    <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
+                        <Modal.Header>
+                            <Modal.Title>Delete Person</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>Are you sure you want to delete {team.name}?</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={() => setShowDeleteModal(false)}>
+                                Cancel
+                            </Button>
+                            <Button variant="danger" onClick={() => handleDelete(team.id)}>
+                                Delete
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
                 )
             })
             return (<Row>{rowItems}</Row>)
