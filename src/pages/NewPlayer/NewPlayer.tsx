d6eac52c5e552a6abd83e6abf43f933c95d1e22a
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-07-30 15:42:20 -0400

    get person from id on new-player form

--- src/pages/NewPlayer/NewPlayer.tsx
+++ src/pages/NewPlayer/NewPlayer.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import { Person, useGoldCupApi } from "../../hooks"
+
 export const NewPlayer = () => {
+    const [searchParams,] = useSearchParams()
+    const { getPerson, createCookieObject } = useGoldCupApi()
+    const [person, setPerson] = useState<Person | null>(null)
+
+    useEffect(() => {
+        const personId = searchParams.get('person-id')
+        const token = createCookieObject().token
+        if (personId && token) {
+            getPerson(token, Number(personId)).then(res => setPerson(res.data))
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
-        <h1>New Player</h1>
+        <>
+        <h1>New Player for {person?.first_name}</h1>
+        </>
     )
 }
