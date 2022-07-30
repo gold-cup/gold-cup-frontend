8dc16fc45164878c38239ab92f45ac160a4bdc5c
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-07-30 09:52:36 -0400

    Added New Player Button

--- src/pages/Dashboard/components/PlayerManagement/PlayerManagement.tsx
+++ src/pages/Dashboard/components/PlayerManagement/PlayerManagement.tsx
@@ -1,11 +1,16 @@
+import { Stack, Button } from "react-bootstrap"
+import { useNavigate } from "react-router"
 import { PlayersList } from "./components"
 
 export const PlayerManagement = () => {
-
+    const navigate = useNavigate()
 
     return (
         <>
-            <h1>Player Management</h1>
+            <Stack direction='horizontal' gap={3}>
+                <h3>Player Management</h3>
+                <Button className='ms-auto' onClick={() => navigate("../new-player")}>Add Person</Button>
+            </Stack>
             <p>Here is where you can register yourself or others to be players in gold cup.</p>
             <PlayersList />
         </>
